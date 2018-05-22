b98db618f52c8ac0e90322437ec6377d2f4202bd
Dyego Cruz <dyegocruz@gmail.com>
2018-05-22 17:26:49 -0300

    Organizing imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
-import Container from './Container';
-
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
 import 'modules/devicons/css/devicons.min.css';
 import 'modules/simple-line-icons/css/simple-line-icons.css';
 
+import React from 'react';
+import Container from './Container';
 import './assets/css/app.css';
 
 import Nav from './Nav';

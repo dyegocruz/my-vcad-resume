2d1ab7850510b22f000287bad566bbb7600e1e80
Dyego Cruz <dyegocruz@gmail.com>
2018-05-22 13:42:08 -0300

    Add bootstrap and jquery.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const title = 'My vcard resume';
+import App from './App';
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <App />,
   document.getElementById('app'),
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: [
     'react-hot-loader/patch',
@@ -19,7 +22,19 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
+    alias: {
+      modules: `${__dirname}/node_modules`,
+      jquery: 'modules/jquery/dist/jquery.min.js',
+      bootstrap: 'modules/bootstrap/dist/js/bootstrap.min.js',
+    },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
   output: {
     path: `${__dirname}/dist`,
     publicPath: '/',

6fcfde032cecea579776485b0d0fce9f87494d44
Dyego Cruz <dyegocruz@gmail.com>
2018-05-22 10:42:38 -0300

    Initial config.

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'My test application poxa vida que legal IEI asdasdasd';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app'),
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.jsx',
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};

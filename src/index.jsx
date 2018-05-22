2309dcf8679bd7c00c0449c0dd58e5ce62c1931e
Dyego Cruz <dyegocruz@gmail.com>
2018-05-22 11:39:53 -0300

    Config index.html and manifest.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = 'My test application poxa vida que legal IEI asdasdasd';
+const title = 'My vcard resume';
 
 ReactDOM.render(
   <div>{title}</div>,
   document.getElementById('app'),
 );
-

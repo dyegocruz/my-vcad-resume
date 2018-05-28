7d01fc81e868e77872b019e1d92258a93d6b0097
Dyego Cruz <dyegocruz@gmail.com>
2018-05-28 10:39:38 -0300

    Conclusion of Experience page.

--- src/myVcard/components/Experience.jsx
+++ src/myVcard/components/Experience.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default () => (
-  <div className="my-auto">
-    <h2 className="mb-5">Experience</h2>
+class Experience extends Component {
+  renderSummary(list) {
+    return list.map(item => (
+      <li key={Math.random()}>{item}</li>
+    ));
+  }
 
-    <div className="resume-item d-flex flex-column flex-md-row mb-5">
-      <div className="resume-content mr-auto">
-        <h3 className="mb-0">Senior Web Developer</h3>
-        <div className="subheading mb-3">Intelitec Solutions</div>
-        <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
-      </div>
-      <div className="resume-date text-md-right">
-        <span className="text-primary">March 2013 - Present</span>
-      </div>
-    </div>
+  renderEndDate(position) {
+    if (position.isCurrent) {
+      return (<span>Present</span>);
+    }
+    return <span>{position.endDate.month} {position.endDate.year}</span>;
+  }
 
-    <div className="resume-item d-flex flex-column flex-md-row mb-5">
-      <div className="resume-content mr-auto">
-        <h3 className="mb-0">Web Developer</h3>
-        <div className="subheading mb-3">Intelitec Solutions</div>
-        <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+  renderPositions() {
+    const list = this.props.positions || [];
+    return list.map(position => (
+      <div key={position.id} className="resume-item d-flex flex-column flex-md-row mb-5">
+        <div className="resume-content mr-auto">
+          <h3 className="mb-0">{position.title}</h3>
+          <div className="subheading mb-3">{position.company}</div>
+          <ul>
+            {this.renderSummary(position.summary)}
+          </ul>
+          <p><strong>Environment</strong>: {position.environment}</p>
+        </div>
+        <div className="resume-date text-md-right">
+          <span className="text-primary">
+            {position.startDate.month} {position.startDate.year} - {this.renderEndDate(position)}
+          </span>
+        </div>
       </div>
-      <div className="resume-date text-md-right">
-        <span className="text-primary">December 2011 - March 2013</span>
-      </div>
-    </div>
+    ));
+  }
 
-    <div className="resume-item d-flex flex-column flex-md-row mb-5">
-      <div className="resume-content mr-auto">
-        <h3 className="mb-0">Junior Web Designer</h3>
-        <div className="subheading mb-3">Shout! Media Productions</div>
-        <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+  render() {
+    return (
+      <div className="my-auto">
+        <h2 className="mb-5">Experience</h2>
+        {this.renderPositions()}
       </div>
-      <div className="resume-date text-md-right">
-        <span className="text-primary">July 2010 - December 2011</span>
-      </div>
-    </div>
+    );
+  }
+}
 
-    <div className="resume-item d-flex flex-column flex-md-row">
-      <div className="resume-content mr-auto">
-        <h3 className="mb-0">Web Design Intern</h3>
-        <div className="subheading mb-3">Shout! Media Productions</div>
-        <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
-      </div>
-      <div className="resume-date text-md-right">
-        <span className="text-primary">September 2008 - June 2010</span>
-      </div>
-    </div>
-  </div>
-)
+const mapStateToProps = state => ({ positions: state.myVcardReducer.myVcard.positions });
+export default connect(mapStateToProps)(Experience);

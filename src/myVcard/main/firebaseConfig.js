41c8deb32e330d9a44a22c7d0ab6b3b53067e09d
Dyego Cruz <dyegocruz@gmail.com>
2018-05-28 14:07:16 -0300

    Convert style code to es6.

--- src/myVcard/main/firebaseConfig.js
+++ src/myVcard/main/firebaseConfig.js
@@ -1,12 +1,11 @@
 import env from '../../../.env.local';
 
-module.exports = {
-  rrfConfig: { userProfile: 'users' },
-  connConfig: {
-    apiKey: env.firebase.apiKey,
-    authDomain: env.firebase.authDomain,
-    databaseURL: env.firebase.databaseURL,
-    storageBucket: env.firebase.storageBucket,
-    messagingSenderId: env.firebase.messagingSenderId,
-  },
+export const rrfConfig = { userProfile: 'users' };
+
+export const connConfig = {
+  apiKey: env.firebase.apiKey,
+  authDomain: env.firebase.authDomain,
+  databaseURL: env.firebase.databaseURL,
+  storageBucket: env.firebase.storageBucket,
+  messagingSenderId: env.firebase.messagingSenderId,
 };

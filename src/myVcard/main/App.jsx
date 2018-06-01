62bd635799a425f6ebcfd8f89cc306d7311921cf
Dyego Cruz <dyegocruz@gmail.com>
2018-06-01 17:07:38 -0300

    Fixing tracking analytics.

--- src/myVcard/main/App.jsx
+++ src/myVcard/main/App.jsx
@@ -4,22 +4,30 @@ import 'modules/devicons/css/devicons.min.css';
 import 'modules/simple-line-icons/css/simple-line-icons.css';
 
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import ReactGA from 'react-ga';
 
 import { uiGA } from '../../consts';
 import '../../assets/scss/app.scss';
 import Container from '../components/Container';
 
+const history = createBrowserHistory();
+history.listen((location) => {
+  window.ga('set', 'page', location.pathname + location.search);
+  window.ga('send', 'pageview', location.pathname + location.search);
+});
+
 // initialize Google Analytics
 ReactGA.initialize(uiGA.code);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function fireTracking() {
   ReactGA.pageview(window.location.hash);
 }
 
 export default () => (
-  <Router onUpdate={fireTracking}>
+  <Router onUpdate={fireTracking} history={history}>
     <Route exact path="/" component={Container} />
   </Router>
 );
